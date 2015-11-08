c147361a3af1f97b641f7e92f2942dd46f6411f9
xingliang <xliang22@asu.edu>
2015-11-08 14:15:17 -0700

    no changes

--- src/CalcAttributesThread.java
+++ src/CalcAttributesThread.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class CalcAttributesThread implements Runnable {
@@ -32,6 +33,12 @@ public class CalcAttributesThread implements Runnable {
 			if(type.equals("ModalStd") || type.equals("TimeStd")){
 				this.results[i] = computeTimeOrModalStd(scarcities);
 			}
+			if(type.equals("ModalRMS") ||  type.equals("TimeRMS")){
+				this.results[i] = computeTimeOrModalRMS(scarcities);
+			}
+			if(type.equals("ModalCV") || type.equals("ModalCV")){
+				this.results[i] = computeTimeOrModalCV(scarcities);
+			}
 		}
 	}
 	
@@ -60,7 +67,7 @@ public class CalcAttributesThread implements Runnable {
 			for(int i=0; i<scarcities.length; i++){
 				if(scarcities[i] != -1){
 					nonneg++;
-					sqrsum+= Math.pow(scarcities[i]-mean, 2.0);
+					sqrsum += Math.pow(scarcities[i]-mean, 2.0);
 				}
 			}
 			if(nonneg == 0)
@@ -70,5 +77,64 @@ public class CalcAttributesThread implements Runnable {
 		}
 	}
 	
+	private double computeTimeOrModalRMS(double[] scarcities){
+		double mean = computeTimeOrModalMean(scarcities);
+		if(mean == -1)
+			return -1;
+		else{
+			double sqrsum = 0;
+			double nonneg = 0;
+			for(int i=0; i<scarcities.length; i++){
+				if(scarcities[i] != -1){
+					nonneg++;
+					sqrsum += Math.pow(scarcities[i], 2.0);
+				}
+			}
+			if(nonneg == 0)
+				return -1;
+			else
+				return Math.sqrt(sqrsum/nonneg);	
+		}
+	}
+	
+	private double computeTimeOrModalCV(double[] scarcities){
+		double mean = computeTimeOrModalMean(scarcities);
+		if(mean == -1)
+			return -1;
+		double std = computeTimeOrModalStd(scarcities);
+		if(std == -1)
+			return -1;
+		return std/mean;
+	}
+	
+	private double computeTimeOrModalEntropy(double[] scarcities){
+		int type = 4;
+		double[] occurences = new double[type];
+		double entropy = 0;
+		double nonzero = 0;
+		for(double val : scarcities){
+			if(val>1700)
+				occurences[3]++;
+			if(val<=1700 && val>1000)
+				occurences[2]++;
+			if(val<=1000 && val>500)
+				occurences[1]++;
+			if(val<=500 && val>0)
+				occurences[0]++;
+		}
+		occurences[0] = occurences[0]/scarcities.length;
+		occurences[1] = occurences[1]/scarcities.length;
+		occurences[2] = occurences[2]/scarcities.length;
+		occurences[3] = occurences[3]/scarcities.length;
+		for(int i=0; i<occurences.length; i++){
+			if(occurences[i]!=0){
+				entropy+=occurences[i]*Math.log(occurences[i]);
+				nonzero++;
+			}
+		}
+		entropy = entropy/Math.log(nonzero);
+		double MaxEntropy = Math.log(scarcities.length)/Math.log(nonzero);
+		return entropy/(MaxEntropy-0);
+	}
 	
 }
